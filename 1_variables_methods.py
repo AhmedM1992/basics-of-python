bd4a652986de8d30f6290861adc2f02e035c0532
ahmed_alkanan <ahmedmahdi5@yahoo.com>
2018-06-23 16:20:49 +0300

    add vars and methods

---
+++ 1_variables_methods.py
@@ -0,0 +1,52 @@
+# Variables
+
+
+a = 15
+
+b = 10
+
+my_var = 55
+
+my_string = "hello"
+
+single_quotes = 'Hello, my name is Ahmed'
+
+var1 = 'hi'
+var2 = 'hi'
+
+# Print variables
+
+
+print(my_string)
+print(a)
+print(a*b)
+print(var1)
+print(var2)
+
+print('Hello world!')
+print(2018)
+
+
+# Methods
+
+
+def my_first_func(my_argument):
+    print(my_argument)
+
+
+my_first_func("This is my argument")
+print(my_first_func('Hello, this is argument too'))
+
+
+def my_multiply_func(num1, num2):
+    return num1 * num2
+
+
+result = my_multiply_func(3, 2)
+print(result)
+print(my_multiply_func(5, 6))
+
+
+
+
+
